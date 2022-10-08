edd28493a95ea915032bb6192a18c49c46edcd0a
sdott3 <sdotthomas347@gmail.com>
2022-10-08 12:30:49 -0400

    added nav component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Nav from './components/Nav';
 import About from './components/About';
 
 function App() {
   return (
     <div>
+      <Nav />
       <main className="text-3xl text-gray-400 font-bold">
         <About />
       </main>
